a0244e6f0222211eed25b594d6d563342aa275ce
Esther Joseph <60444776+esther-joseph@users.noreply.github.com>
2025-04-02 21:21:51 -0500

    create App.js

---
+++ App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Header from './components/Header';
+import RecipeList from './components/RecipeList';
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <RecipeList />
+    </div>
+  );
+}
+
+export default App;
