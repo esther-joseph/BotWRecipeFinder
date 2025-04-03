1b84bbad49814a3d2d80bb6f7f51edebd4bc123a
Esther Joseph <60444776+esther-joseph@users.noreply.github.com>
2025-04-02 21:25:32 -0500

    create Header.js

---
+++ components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Header() {
+  return (
+    <header>
+      <h1>BOTW Recipe Finder</h1>
+      <p>Discover recipes with ingredients you’ve gathered!</p>
+    </header>
+  );
+}
+
+export default Header;
